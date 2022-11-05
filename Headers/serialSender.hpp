fc63c9119479a35e9a1d1d9bccdacbc954866d7f
Diabltica <Diabltica@gmail.com>
2022-11-05 18:08:09 +0100

    first branch commit

---
+++ Headers/serialSender.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Diabltica on 05/11/2022.
+//
+
+#ifndef FOCUS_REMOTE_SERIALSENDER_HPP
+#define FOCUS_REMOTE_SERIALSENDER_HPP
+
+#endif // FOCUS_REMOTE_SERIALSENDER_HPP
---
+++ src/serialSender.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Diabltica on 05/11/2022.
+//
+
+#include "Headers/serialSender.hpp"
